52b8c4f1eab5393513d6c2002c770b8d6d77b128
m-goda <m.goda@wakeb.tech>
2023-08-06 10:18:21 +0200

    fix

--- vue/src/store/organizations/mutations.js
+++ vue/src/store/organizations/mutations.js
@@ -2,12 +2,9 @@ const mutations = {
   SET_ORGANIZATIONS(state, value) {
     state.organizations = value
   },
-<<<<<<< HEAD
   SET_ORGANIZATION(state, value) {
     state.organization = value
   }
-=======
->>>>>>> 067342234c9ecdbc758a74d5845dc50b5d1d0529
 }
 
 export default mutations
