5d350fc6c17ffb66bb0a8395ba99a18266ee3054
m-khairy <m.khairy@wakeb.tech>
2023-08-09 13:28:56 +0300

    make activity log

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,13 +5,24 @@ namespace App\Models;
 use App\Enums\TaskTypeEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Task extends Model
 {
     use HasFactory;
+    use LogsActivity;
+
+    public $inPermission = true;
+
     protected $fillable = [
         'title', 'type', 'user_id', 'assigner_id', 'due_date', 'details', 'document_id', 'share_with', 'form_id',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logOnly(self::getFillable());
+    }
 
     // public function getTypeAttribute($value)
     // {
--- app/Models/document.php
+++ app/Models/document.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class document extends Model
 {
     use HasFactory;
+    use LogsActivity;
+
     protected $fillable = ['name', 'status', 'type', 'user_id'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logOnly(self::getFillable());
+    }
 
     public function user()
     {
