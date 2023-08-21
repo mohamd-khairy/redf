58f64a75d69c29a910167fe3ab895660d9c21b5c
m-khairy <m.khairy@wakeb.tech>
2023-08-21 09:34:20 +0300

    fix

---
+++ app/Http/Requests/FormFillRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * @property mixed name
+ * @property mixed pages
+ */
+class FormFillRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'form_request_number' => 'required|string|unique:forms,form_request_number,' . request()->form_id,
+        ];
+    }
+}
