7f0bd16ad339f0b1c8c4916d98293a9ad2352bb3
MoazG <moaz120560@gmail.com>
2023-08-22 12:13:00 +0300

    fix edit

--- vue/src/pages/dashboard/Users/Main.vue
+++ vue/src/pages/dashboard/Users/Main.vue
@@ -1,34 +1,41 @@
 <template>
-    <div class="d-flex flex-column flex-grow-1">
-        <v-card>
-            <!-- templates list -->
-            <v-row dense class="pa-2 align-center">
-                <v-col cols="6">
-                    <v-menu offset-y left>
-                        <template v-slot:activator="{ on }">
-                            <transition name="slide-fade" mode="out-in">
-                                <v-btn v-show="selectedtemplates.length > 0" v-on="on">
-                                    {{ $t("general.actions") }}
-                                    <v-icon right>mdi-menu-down</v-icon>
-                                </v-btn>
-                            </transition>
-                        </template>
-                        <v-list dense>
+  <div class="d-flex flex-column flex-grow-1">
+    <v-card>
+      <!-- templates list -->
+      <v-row dense class="pa-2 align-center">
+        <v-col cols="6">
+          <v-menu offset-y left>
+            <template v-slot:activator="{ on }">
+              <transition name="slide-fade" mode="out-in">
+                <v-btn v-show="selectedtemplates.length > 0" v-on="on">
+                  {{ $t("general.actions") }}
+                  <v-icon right>mdi-menu-down</v-icon>
+                </v-btn>
+              </transition>
+            </template>
+            <v-list dense>
+              <v-list-item>
+                <v-list-item-title>{{
+                  $t("general.delete")
+                }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+        <v-col cols="6" class="d-flex text-right align-center">
+          <v-text-field
+            v-model="searchQuery"
+            append-icon="mdi-magnify"
+            class="flex-grow-1 mr-md-2"
+            solo
+            hide-details
+            dense
+            clearable
+            :placeholder="$t('general.search')"
+            @keyup.enter="searchtemplate(searchQuery)"
+          ></v-text-field>
 
-                            <v-list-item>
-                                <v-list-item-title>{{
-                                    $t("general.delete")
-                                }}</v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
-                </v-col>
-                <v-col cols="6" class="d-flex text-right align-center">
-                    <v-text-field v-model="searchQuery" append-icon="mdi-magnify" class="flex-grow-1 mr-md-2" solo
-                        hide-details dense clearable :placeholder="$t('general.search')"
-                        @keyup.enter="searchtemplate(searchQuery)"></v-text-field>
-
-                    <!-- <v-tooltip top>
+          <!-- <v-tooltip top>
                         <template v-slot:activator="{ on, attrs }">
                             <v-btn color="primary" class="mx-2 " elevation="0" v-bind="attrs" v-on="on"
                                 to="/templates/types/create" v-can="'create-user'">
@@ -39,25 +46,40 @@
                         </template>
                         <span>{{ $t("templates.createTemplate") }}</span>
                     </v-tooltip> -->
-                    <v-btn color="primary" elevation="0" :loading="isLoading" @click.prevent="open()" class="mx-2">
-                        <v-icon>mdi-refresh</v-icon>
-                    </v-btn>
-                </v-col>
-            </v-row>
-            <v-data-table show-select v-model="selectedtemplates" :headers="headers" :items="templateItems"
-                :options.sync="options" class="flex-grow-1" :loading="isLoading" :page="page" :pageCount="numberOfPages"
-                :server-items-length="totaltemplates">
-                <template v-slot:item.id="{ item }">
-                    <div class="font-weight-bold">
-                        # <copy-label :text="templateItems.indexOf(item) + 1 + ''" />
-                    </div>
-                </template>
+          <v-btn
+            color="primary"
+            elevation="0"
+            :loading="isLoading"
+            @click.prevent="open()"
+            class="mx-2"
+          >
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-data-table
+        show-select
+        v-model="selectedtemplates"
+        :headers="headers"
+        :items="templateItems"
+        :options.sync="options"
+        class="flex-grow-1"
+        :loading="isLoading"
+        :page="page"
+        :pageCount="numberOfPages"
+        :server-items-length="totaltemplates"
+      >
+        <template v-slot:item.id="{ item }">
+          <div class="font-weight-bold">
+            # <copy-label :text="templateItems.indexOf(item) + 1 + ''" />
+          </div>
+        </template>
 
-                <template v-slot:item.created_at="{ item }">
-                    <div>{{ item.created_at | formatDate("lll") }}</div>
-                </template>
+        <template v-slot:item.created_at="{ item }">
+          <div>{{ item.created_at | formatDate("lll") }}</div>
+        </template>
 
-                <!-- <template v-slot:item.actions="{ item }">
+        <!-- <template v-slot:item.actions="{ item }">
                     <div class="actions">
                         <v-btn color="primary" icon :to="`/templates/types/edit/${item.id}`">
                             <v-icon>mdi-pencil</v-icon>
@@ -67,180 +89,183 @@
                         </v-btn>
                     </div>
                 </template> -->
-                <template v-slot:no-data>
-                    <div class="text-center my-2 primary--text" color="primary">
-                        <emptyDataSvg></emptyDataSvg>
-                        <div class="dt-no_data">
-                            {{ $t("general.no_data_available") }}
-                        </div>
-                    </div>
-                </template>
-            </v-data-table>
-        </v-card>
-    </div>
+        <template v-slot:no-data>
+          <div class="text-center my-2 primary--text" color="primary">
+            <emptyDataSvg></emptyDataSvg>
+            <div class="dt-no_data">
+              {{ $t("general.no_data_available") }}
+            </div>
+          </div>
+        </template>
+      </v-data-table>
+    </v-card>
+    <assign v-model="dialog" :id="formId"></assign>
+  </div>
 </template>
 
 <script>
-import $ from 'jquery'
+import $ from "jquery";
 import CopyLabel from "@/components/common/CopyLabel";
 import { mapActions, mapState } from "vuex";
 import { ask, makeToast } from "@/helpers";
+import Assign from "../../../components/cases/Assign";
 import emptyDataSvg from "@/assets/images/illustrations/empty-data.svg";
 export default {
-    components: {
-        CopyLabel,
-        emptyDataSvg
-    },
-    data() {
-        return {
-            page: 1,
-            totaltemplates: 0,
-            numberOfPages: 0,
-            options: {},
-            isLoading: false,
-            breadcrumbs: [
-                {
-                    text: this.$t("menu.dashboard"),
-                    disabled: false,
-                    href: "/"
-                },
-                {
-                    text: this.$t("menu.beneficiaries")
-                }
-            ],
-
-            searchQuery: "",
-            selectedtemplates: [],
-            templateItems: [],
-            headers: [
-                {
-                    text: this.$t("tables.id"),
-                    value: "id"
-                },
-                {
-                    text: this.$t("tables.name"),
-                    value: "name"
-                },
-                {
-                    text: this.$t("tables.type"),
-                    value: "type"
-                },
-                {
-                    text: this.$t("tables.email"),
-                    value: "email"
-                },
-                {
-                    text: this.$t("tables.created"),
-                    value: "created_at"
-                },
-                // {
-                //     text: this.$t("tables.actions"),
-                //     value: "actions"
-                // },
-
-            ],
-        };
-    },
-    watch: {
-        selectedTemplates(val) { },
-        options: {
-            handler() {
-                this.open();
-            }
+  components: {
+    CopyLabel,
+    emptyDataSvg,
+    Assign,
+  },
+  data() {
+    return {
+      page: 1,
+      dialog: false,
+      totaltemplates: 0,
+      numberOfPages: 0,
+      options: {},
+      isLoading: false,
+      breadcrumbs: [
+        {
+          text: this.$t("menu.dashboard"),
+          disabled: false,
+          href: "/",
         },
-        deep: true,
-        searchQuery() {
-            this.open();
-        }
+        {
+          text: this.$t("menu.beneficiaries"),
+        },
+      ],
+
+      searchQuery: "",
+      selectedtemplates: [],
+      templateItems: [],
+      headers: [
+        {
+          text: this.$t("tables.id"),
+          value: "id",
+        },
+        {
+          text: this.$t("tables.name"),
+          value: "name",
+        },
+        {
+          text: this.$t("tables.type"),
+          value: "type",
+        },
+        {
+          text: this.$t("tables.email"),
+          value: "email",
+        },
+        {
+          text: this.$t("tables.created"),
+          value: "created_at",
+        },
+        // {
+        //     text: this.$t("tables.actions"),
+        //     value: "actions"
+        // },
+      ],
+    };
+  },
+  watch: {
+    selectedTemplates(val) {},
+    options: {
+      handler() {
+        this.open();
+      },
     },
-    computed: {
-        ...mapState("users", ["beneficiaries"])
+    deep: true,
+    searchQuery() {
+      this.open();
     },
-    created() {
-        this.setBreadCrumb({
-            breadcrumbs: this.breadcrumbs,
-            pageTitle: this.$t("menu.beneficiaries")
+  },
+  computed: {
+    ...mapState("users", ["beneficiaries"]),
+  },
+  created() {
+    this.setBreadCrumb({
+      breadcrumbs: this.breadcrumbs,
+      pageTitle: this.$t("menu.beneficiaries"),
+    });
+  },
+  methods: {
+    ...mapActions("users", ["getBeneficiaries"]),
+    ...mapActions("app", ["setBreadCrumb"]),
+    searchtemplate() {},
+    open() {
+      this.isLoading = true;
+      let { page, itemsPerPage } = this.options;
+      const direction = this.options.sortDesc[0] ? "asc" : "desc";
+
+      let data = {
+        search: this.searchQuery,
+        pageSize: itemsPerPage,
+        pageNumber: page,
+        sortDirection: direction,
+        sortColumn: this.options.sortBy[0] ?? "",
+      };
+      this.getBeneficiaries(data)
+        .then(() => {
+          data;
+          this.isLoading = false;
+          if (itemsPerPage != -1) {
+            this.templateItems = this.beneficiaries.data;
+            this.totaltemplates = this.beneficiaries.total;
+            this.numberOfPages = this.beneficiaries.last_page;
+          } else {
+            this.templateItems = this.beneficiaries;
+            this.totaltemplates = this.beneficiaries.length;
+            this.numberOfPages = 1;
+          }
+          console.log(this.beneficiaries);
+        })
+        .catch(() => {
+          this.isLoading = false;
         });
     },
-    methods: {
-        ...mapActions("users", ["getBeneficiaries"]),
-        ...mapActions("app", ["setBreadCrumb"]),
-        searchtemplate() { },
-        open() {
-            this.isLoading = true;
-            let { page, itemsPerPage } = this.options;
-            const direction = this.options.sortDesc[0] ? "asc" : "desc";
 
-            let data = {
-                search: this.searchQuery,
-                pageSize: itemsPerPage,
-                pageNumber: page,
-                sortDirection: direction,
-                sortColumn: this.options.sortBy[0] ?? ""
-            };
-            this.getBeneficiaries(data)
-                .then(() => {
-                    data
-                    this.isLoading = false;
-                    if (itemsPerPage != -1) {
-                        this.templateItems = this.beneficiaries.data;
-                        this.totaltemplates = this.beneficiaries.total;
-                        this.numberOfPages = this.beneficiaries.last_page;
-                    } else {
-                        this.templateItems = this.beneficiaries;
-                        this.totaltemplates = this.beneficiaries.length;
-                        this.numberOfPages = 1;
-                    }
-                    console.log(this.beneficiaries);
-                })
-                .catch(() => {
-                    this.isLoading = false;
-                });
-        },
-
-        async deleteAlltemplates() {
-            let data = {};
-            let ids = [];
-            const { isConfirmed } = await ask("Are you sure to delete it?", "info");
-            if (isConfirmed) {
-                if (this.selectedTemplates.length) {
-                    this.selectedTemplates.forEach(item => {
-                        ids.push(item.id);
-                    });
-                }
-                data = {
-                    ids: ids,
-                    action: "delete",
-                    value: 1
-                };
-                this.isLoading = true;
-                this.deleteAll(data)
-                    .then(response => {
-                        makeToast("success", response.data.message);
-                        this.open();
-                        this.isLoading = false;
-                    })
-                    .catch(() => {
-                        this.isLoading = false;
-                    });
-            }
+    async deleteAlltemplates() {
+      let data = {};
+      let ids = [];
+      const { isConfirmed } = await ask("Are you sure to delete it?", "info");
+      if (isConfirmed) {
+        if (this.selectedTemplates.length) {
+          this.selectedTemplates.forEach((item) => {
+            ids.push(item.id);
+          });
         }
-    }
+        data = {
+          ids: ids,
+          action: "delete",
+          value: 1,
+        };
+        this.isLoading = true;
+        this.deleteAll(data)
+          .then((response) => {
+            makeToast("success", response.data.message);
+            this.open();
+            this.isLoading = false;
+          })
+          .catch(() => {
+            this.isLoading = false;
+          });
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .slide-fade-enter-active {
-    transition: all 0.3s ease;
+  transition: all 0.3s ease;
 }
 
 .slide-fade-leave-active {
-    transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
 .slide-fade-enter,
 .slide-fade-leave-to {
-    transform: translateX(10px);
-    opacity: 0;
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
