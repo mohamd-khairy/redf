f0865c94cbf5b02b374cae35662ab9d5aeb74e05
mostafaabdelfatah <mustafa.salama2608@gmail.com>
2023-08-23 15:58:20 +0300

    edit in database

--- database/migrations/2023_08_16_113207_create_user_information_table.php
+++ database/migrations/2023_08_16_113207_create_user_information_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('user_information', function (Blueprint $table) {
             $table->id();
-            $table->integer('civil_number');
+            $table->bigInteger('civil_number');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
