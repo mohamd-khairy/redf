c13d70ff572124b5290055d7919d90cfe630c895
MoazG <moaz120560@gmail.com>
2023-08-26 12:10:46 +0300

    fix time format

--- vue/src/filters/formatDate.js
+++ vue/src/filters/formatDate.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import moment from 'moment-timezone'
-import store from '../store'
+import Vue from "vue";
+import moment from "moment-timezone";
+import store from "../store";
 
-Vue.filter('formatDate', (value, filterFormat) => {
-  const { zone, format } = store.state.app.time
+Vue.filter("formatDate", (value, filterFormat) => {
+  const { zone, format } = store.state.app.time;
 
   if (value) {
-    return moment(value).tz(zone).format(filterFormat || format || 'lll')
+    return moment(value)
+      .tz(zone)
+      .format(filterFormat || format || "lll");
   }
 
-  return ''
-})
+  return "";
+});
