bc472d01b6d40c91257ed6a0b52575ba54cf7e18
m-khairy <m.khairy@wakeb.tech>
2023-09-03 15:22:22 +0300

    fix

--- app/Enums/FormEnum.php
+++ app/Enums/FormEnum.php
@@ -7,7 +7,7 @@ enum FormEnum: int
     case CASE = 1;                     // قضيه
     case LEGAL_ADVICE = 2;             // استشاره قانونيه
     case LEGAL_REVIEW = 3;             // مراجعه
-    case DEFENCE_FORM = 4;             // دفاع
+    case DEFENCE_CASE_FORM = 4;             // دفاع
     case CLAIM_CASE_FORM = 5;          // رفع دعوي
     case RESUME_CASE_FORM = 6;         // استئناف
     case SOLICITATION_CASE_FORM = 7;   // التماس
