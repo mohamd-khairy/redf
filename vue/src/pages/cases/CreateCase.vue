8435f17018a521e45af693a1bc6ec51477983c57
Enas Magdy <enasmagdy96@gmail.com>
2023-09-03 17:12:32 +0300

    get branches

--- vue/src/pages/cases/CreateCase.vue
+++ vue/src/pages/cases/CreateCase.vue
@@ -530,6 +530,7 @@ export default {
     this.init();
     this.fetchUsers();
     this.fetchDepartments();
+    this.getBranches({});
 
     this.$root.$on("userCreated", () => {
       this.fetchUsers();
@@ -539,7 +540,6 @@ export default {
     e1(val) {
       if (val === 3) {
         this.getCourts();
-        this.getBranches({});
       }
     },
   },
