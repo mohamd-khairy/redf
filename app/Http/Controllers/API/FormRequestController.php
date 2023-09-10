0466cf6f2e6b9c25b618df3a653e9e557c36f2de
mostafaabdelfatah <mustafa.salama2608@gmail.com>
2023-09-10 11:06:29 +0300

    validation

--- app/Http/Controllers/API/FormRequestController.php
+++ app/Http/Controllers/API/FormRequestController.php
@@ -7,15 +7,19 @@ use App\Models\Form;
 use App\Models\User;
 use App\Models\FormRequest;
 use Illuminate\Http\Request;
+use App\Rules\StatusEnumRule;
+use App\Models\FormRequestSide;
+use App\Enums\FormRequestStatus;
 use App\Http\Requests\FormAssign;
 use App\Http\Requests\PageRequest;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Services\FormRequestService;
+use Illuminate\Validation\Rules\Enum;
 use App\Http\Requests\FormFillRequest;
 use App\Http\Requests\InformationRequest;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\FormRequestResource;
-use App\Models\FormRequestSide;
 
 class FormRequestController extends Controller
 {
@@ -132,42 +136,22 @@ class FormRequestController extends Controller
             return responseFail($th->getMessage());
         }
     }
-    public function retrieveClaimant2(Request $request){
-        $formRequestSide = DB::table('form_request_sides')
-        ->select('claimant_id', 'defendant_id')
-        ->where('form_request_id', $request->form_request_id)
-        ->first();
 
-         if (!$formRequestSide) {
-            return responseFail('FormRequestSide not found');
-         }
-        $claimant = User::find($formRequestSide->claimant_id);
-        $defendant = User::find($formRequestSide->defendant_id);
-
-        if (!$claimant || !$defendant) {
-            return responseFail('Claimant or defendant not found');
-         }
-        $response = [
-            'claimant' => [
-                'id' => $claimant->id,
-                'name' => $claimant->name,
-            ],
-            'defendant' => [
-                'id' => $defendant->id,
-                'name' => $defendant->name,
-            ],
-        ];
-        return responseSuccess($response, 'Retrieve Claimant And Defendant');
-     }
      public function changeStatus(Request $request){
-        $updateStatus = FormRequest::where('id', $request->form_request_id)
-            ->update(['status' => $request->status]);
 
-        if ($updateStatus === 0) {
-            return responseFail('Form request not found');
+        $validator = Validator::make($request->all(), [
+            'form_request_id' => 'required|exists:form_requests,id',
+             'status' => [new Enum(FormRequestStatus::class)],
+        ]);
+
+        if ($validator->fails()) {
+            return responseFail($validator->errors()->first());
         }
+        $updateStatus = FormRequest::where('id', $request->form_request_id)
+        ->update(['status' => $request->status]);
 
-        return responseSuccess($response, 'Status updated successfully');
+        return responseSuccess('Status updated successfully');
+
      }
     public function retrieveClaimant(Request $request)
     {
