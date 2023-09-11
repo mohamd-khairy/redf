b5d3642f305b98f711b21474f2326b3ac8115e8c
m-khairy <m.khairy@wakeb.tech>
2023-09-11 10:50:29 +0300

    fix

--- app/Helpers/Model.php
+++ app/Helpers/Model.php
@@ -1,6 +1,7 @@
 <?php
 
-
+use App\Enums\CaseTypeEnum;
+use App\Enums\FormRequestStatus;
 use App\Models\User;
 use App\Enums\UserTypeEnum;
 
@@ -8,5 +9,25 @@ if (!function_exists('userType')) {
     function userType($type = UserTypeEnum::EMPLOYEE)
     {
         return User::where('type', $type)->get();
+    }
+}
+
+
+if (!function_exists('DisplayStatus')) {
+    function DisplayStatus($value)
+    {
+        try {
+            $status = $value ? CaseTypeEnum::$value() : null;
+        } catch (\Throwable $th) {
+            $status = null;
+        }
+
+        try {
+            $status = $status ?  $status : FormRequestStatus::$value();
+        } catch (\Throwable $th) {
+            $status = $value;
+        }
+
+        return $status;
     }
 }
