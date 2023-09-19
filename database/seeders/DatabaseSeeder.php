50e0ef09f33ae4c42d63fad5b87ceafff9529041
m-khairy <m.khairy@wakeb.tech>
2023-09-19 14:21:43 +0300

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,10 +22,10 @@ class DatabaseSeeder extends Seeder
                 $this->call(CourtSeeder::class);
                 $this->call(DepartmentSeeder::class);
                 $this->call(SettingSeeder::class);
-                $this->call(TemplateSeeder::class);
                 $this->call(FormSeeder::class);
                 $this->call(CaseRelatedSeeder::class);
                 $this->call(UserRolePermissionSeeder::class);
+                $this->call(TemplateSeeder::class);
                 $this->call(ReportDatabaseSeeder::class);
                 $this->call(StageSeeder::class);
         }
